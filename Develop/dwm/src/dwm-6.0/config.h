1c7815421206292bfd93c2e17c5d1e42a159db44
Christopher Truett <chris.truett@gmail.com>
2012-08-29 08:59:47 -0400

    Again, minor updates to DWM.

--- Develop/dwm/src/dwm-6.0/config.h
+++ Develop/dwm/src/dwm-6.0/config.h
@@ -25,22 +25,24 @@ static const Bool showbar           = True;     /* False means no bar */
 static const Bool topbar            = True;     /* False means bottom bar */
 
 /* tagging */
-static const char *tags[] = { "\u00c2", "\u00C0", "\u00C1" , "\u00C8", "\u00D4 " };
+static const char *tags[] = { "\u00c2", "\u00C0", "\u00C1" , "\u00C8", "\u00CE", "\u00D4 " };
 
 static const Rule rules[] = {
   /* class      instance    title       tags mask     isfloating   monitor */
-  // { NULL    , NULL        , NULL        , 0    , True  , -1 } ,
-  { "URxvt "   , NULL        , NULL        , 0    , False , -1 } ,
-  { "URxvt"    , NULL        , "weechat"   , 1<<2 , False , -1 } ,
-  { "URxvt"    , NULL        , "pyradio"   , 1<<4 , False , -1 } ,
-  { "URxvt"    , NULL        , "alsamixer" , 1<<4 , False , -1 } ,
-  { "Vlc"      , NULL        , NULL        , 1<<3 , False , -1 } ,
-  { "Firefox"  , "Browser"   , NULL        , 0    , True  , -1 } ,
-  { "Firefox"  , "Dialog"    , NULL        , 0    , True  , -1 } ,
-  { "Firefox"  , "Places"    , NULL        , 0    , True  , -1 } ,
-  { "Firefox"  , "Navigator" , NULL        , 1<<1 , False , -1 } ,
-  { "slgui"    , NULL        , NULL        , 1<<2 , False , -1 } ,
-  { "Qjackctl" , NULL        , NULL        , 1<<3 , True  , -1 } ,
+  // { NULL    , NULL        , NULL           , 0    , True  , -1 } ,
+  { "URxvt "   , NULL        , "chris@arch:~" , 1<<0 , False , -1 } ,
+  { "URxvt"    , NULL        , "weechat"      , 1<<2 , False , -1 } ,
+  { "URxvt"    , NULL        , "weechat"      , 1<<2 , False , -1 } ,
+  { "URxvt"    , NULL        , "htop"         , 1<<5 , False , -1 } ,
+  { "URxvt"    , NULL        , "pyradio"      , 1<<4 , False , -1 } ,
+  { "URxvt"    , NULL        , "alsamixer"    , 1<<5 , False , -1 } ,
+  { "Vlc"      , NULL        , NULL           , 1<<3 , False , -1 } ,
+  { "Firefox"  , "Browser"   , NULL           , 0    , True  , -1 } ,
+  { "Firefox"  , "Dialog"    , NULL           , 0    , True  , -1 } ,
+  { "Firefox"  , "Places"    , NULL           , 0    , True  , -1 } ,
+  { "Firefox"  , "Navigator" , NULL           , 1<<1 , False , -1 } ,
+  { "slgui"    , NULL        , NULL           , 1<<2 , False , -1 } ,
+  { "Qjackctl" , NULL        , NULL           , 1<<3 , True  , -1 } ,
 };
 
 /* layout(s) */
@@ -83,48 +85,49 @@ static const char *rotatewall[] = { "wallpaper-rotate-new", NULL };
 
 #include "movestack.c"
 static Key keys[] = {
-  /* modifier                     key        function        argument */
-  { 0, XF86XK_AudioLowerVolume,   spawn,        { .v = volumedown } },
-  { 0, XF86XK_AudioRaiseVolume,   spawn,        { .v = volumeup } },
-  { 0, XF86XK_AudioMute,          spawn,        { .v = mute } },
-  { MODKEY|ShiftMask,             XK_bracketright, spawn,    {.v = rotatewall } },
-  { MODKEY,                       XK_v,      spawn,          {.v = vlc } },
-  { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-  { MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-  { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-  { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-  // { MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-  // { MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-  { MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-  { MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-  { MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
-  { MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
-  { MODKEY,                       XK_m,      zoom,           {0} },
-  { MODKEY,                       XK_Tab,    view,           {0} },
-  { MODKEY,                       XK_c,      killclient,     {0} },
-  { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-  { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-  { MODKEY,                       XK_Return, setlayout,      {.v = &layouts[2]} },
-  { MODKEY,                       XK_b,      setlayout,      {.v = &layouts[3]} },
-  { MODKEY,                       XK_g,      setlayout,      {.v = &layouts[4]} },
-  { MODKEY,                       XK_space,  setlayout,      {0} },
-  { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-  { MODKEY,                       XK_grave,      view,           {.ui = ~0 } },
-  { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-  { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-  { MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-  { MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-  { MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-    TAGKEYS(                        XK_q,                      0)
-    TAGKEYS(                        XK_w,                      1)
-    TAGKEYS(                        XK_e,                      2)
-    TAGKEYS(                        XK_u,                      3)
-    TAGKEYS(                        XK_i,                      4)
-    TAGKEYS(                        XK_o,                      5)
-    TAGKEYS(                        XK_7,                      6)
-    TAGKEYS(                        XK_8,                      7)
-    TAGKEYS(                        XK_9,                      8)
-    { MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+  /* modifier          key                       function         argument */
+  { 0,                 XF86XK_AudioLowerVolume,  spawn,           { .v = volumedown } },
+  { 0,                 XF86XK_AudioRaiseVolume,  spawn,           { .v = volumeup } },
+  { 0,                 XF86XK_AudioMute,         spawn,           { .v = mute } },
+  { MODKEY|ShiftMask,  XK_bracketright,          spawn,           {.v = rotatewall } },
+  { MODKEY,            XK_v,                     spawn,           {.v = vlc } },
+  { MODKEY,            XK_p,                     spawn,           {.v = dmenucmd } },
+  { MODKEY|ShiftMask,  XK_Return,                spawn,           {.v = termcmd } },
+  { MODKEY,            XK_j,                     focusstack,      {.i = +1 } },
+  { MODKEY,            XK_k,                     focusstack,      {.i = -1 } },
+  // { MODKEY,         XK_i,                     incnmaster,      {.i = +1 } },
+  // { MODKEY,         XK_d,                     incnmaster,      {.i = -1 } },
+  { MODKEY,            XK_h,                     setmfact,        {.f = -0.05} },
+  { MODKEY,            XK_l,                     setmfact,        {.f = +0.05} },
+  { MODKEY|ShiftMask,  XK_j,                     movestack,       {.i = +1 } },
+  { MODKEY|ShiftMask,  XK_k,                     movestack,       {.i = -1 } },
+  { MODKEY,            XK_m,                     zoom,            {0} },
+  { MODKEY,            XK_Tab,                   view,            {0} },
+  { MODKEY|ShiftMask,  XK_q,                     killclient,      {0} },
+  { MODKEY,            XK_t,                     setlayout,       {.v = &layouts[0]} },
+  { MODKEY,            XK_space,                 setlayout,       {.v = &layouts[1]} },
+  { MODKEY,            XK_Return,                setlayout,       {.v = &layouts[2]} },
+  { MODKEY,            XK_b,                     setlayout,       {.v = &layouts[3]} },
+  { MODKEY,            XK_g,                     setlayout,       {.v = &layouts[4]} },
+  { MODKEY,            XK_f,                     setlayout,       {.v = &layouts[5]} },
+  // { MODKEY,            XK_space,                 setlayout,       {0} },
+  { MODKEY|ShiftMask,  XK_space,                 togglefloating,  {0} },
+  { MODKEY,            XK_grave,                 view,            {.ui = ~0 } },
+  // { MODKEY|ShiftMask,  XK_0,                     tag,             {.ui = ~0 } },
+  { MODKEY,            XK_comma,                 focusmon,        {.i = -1 } },
+  { MODKEY,            XK_period,                focusmon,        {.i = +1 } },
+  { MODKEY|ShiftMask,  XK_comma,                 tagmon,          {.i = -1 } },
+  { MODKEY|ShiftMask,  XK_period,                tagmon,          {.i = +1 } },
+    TAGKEYS(                        XK_1,  0)
+    TAGKEYS(                        XK_2,  1)
+    TAGKEYS(                        XK_3,  2)
+    TAGKEYS(                        XK_v,  3)
+    TAGKEYS(                        XK_m,  4)
+    TAGKEYS(                        XK_c,  5)
+    TAGKEYS(                        XK_7,  6)
+    TAGKEYS(                        XK_8,  7)
+    TAGKEYS(                        XK_9,  8)
+  { MODKEY|ControlMask,           XK_q,      quit,           {0} },
 };
 
 
