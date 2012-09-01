68e3f8aca32179371a3fd73898066b97a133e9a9
Christopher Truett <chris.truett@gmail.com>
2012-09-01 03:51:29 -0400

    cleaned up jamendo download script.

---
+++ Develop/python/jamendo_getAlbum.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python2
+# Download jamendo albums --------------------------------------------------------------------------------------------------#
+
+
+# Import necessary libs ----------------------------------------------------------------------------------------------------#
+import urllib
+import argparse
+import json
+import sys
+
+
+# Set up argument parsing --------------------------------------------------------------------------------------------------#
+parser = argparse.ArgumentParser(description='Search jamendo for albums.')
+parser.add_argument("-a", dest="artist", type=str, required=False, help="Search for an artist by name. Required.")
+
+
+# Parse all arguments ------------------------------------------------------------------------------------------------------#
+args = parser.parse_args();
+
+
+# a simple jamendo api urlmap ----------------------------------------------------------------------------------------------#
+urlmap = {
+    "list":      lambda (s): "http://api.jamendo.com/get2/name+id/album/json/?artist_name="+str(s),
+    "album":     lambda (s): "http://www.jamendo.com/get/album/id/album/archiverestricted/redirect/"+str(s)+"/?are=ogg3"
+    }
+
+
+# Get list of albums using artist name -------------------------------------------------------------------------------------#
+def getAlbumList(artist_name):
+
+  query = urlmap["list"](artist_name)
+  f = urllib.urlopen(query)
+  results = json.loads(f.read())
+
+  return [ items for items in results ]
+
+
+# Startup function  --------------------------------------------------------------------------------------------------------#
+def startup():
+
+  global dlink
+
+  def dlProgress(count, blockSize, totalSize):
+    percent = int(count*blockSize*100/totalSize)
+    sys.stdout.write("\r" + dlink + "...%d%%" % percent)
+    sys.stdout.flush()
+
+  albums = getAlbumList(args.artist)
+  for i in range(len(albums)):
+    dlink = urlmap['album'](albums[i]["id"])
+    name = args.artist+"-"+albums[i]["name"]
+    urllib.urlretrieve(dlink, name+".zip", reporthook=dlProgress)
+
+startup()
